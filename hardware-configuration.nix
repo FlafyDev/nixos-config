750c5d248a2b096313fdf2a0ead7e661c162b516
FlafyDev <flafyarazi@gmail.com>
2022-05-30 23:36:48 +0300

    mount

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
@@ -21,6 +21,11 @@
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/4424-E13F";
       fsType = "vfat";
+    };
+
+  fileSystems."/mnt/general" =
+    { device = "/dev/disk/by-uuid/23e60b41-48d2-4b32-8cc8-bf52e0b305f4";
+      fsType = "ext4";
     };
 
   swapDevices =
