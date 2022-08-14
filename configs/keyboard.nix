84abb1a76b5653d46401f817b0cc3097d26bbdb5
FlafyDev <flafyarazi@gmail.com>
2022-08-14 06:26:31 +0300

    change languages with super

--- configs/keyboard.nix
+++ configs/keyboard.nix
@@ -2,7 +2,7 @@
   system = { pkgs, ... }: {
     services.xserver.displayManager.sessionCommands = ''
       ${pkgs.xorg.xset}/bin/xset r rate 200 40
-      ${pkgs.xorg.setxkbmap}/bin/setxkbmap -option caps:escape
+      ${pkgs.xorg.setxkbmap}/bin/setxkbmap setxkbmap -layout us,il -option 'grp:lwin_toggle,caps:escape'
     '';
   };
 }
