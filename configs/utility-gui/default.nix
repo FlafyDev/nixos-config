336cc62c2a0a4b376336ce03f8c355c785242600
FlafyDev <flafyarazi@gmail.com>
2023-04-18 13:33:17 +0300

    gui: add syncplay
    remove webcord and gimp dev

--- configs/utility-gui/default.nix
+++ configs/utility-gui/default.nix
@@ -3,15 +3,12 @@
     nixpkgs-gimp.url = "github:jtojnar/nixpkgs/gimp-meson";
     guifetch.url = "github:flafydev/guifetch";
     listen-blue.url = "github:flafydev/listen_blue";
-    webcord.url = "github:SpacingBat3/webcord";
-    webcord.flake = false;
   };
 
   add = {
     nixpkgs-gimp,
     guifetch,
     listen-blue,
-    webcord,
     ...
   }: {
     overlays = _: [
@@ -23,41 +20,10 @@
         };
       in {
         gimp-dev = pkgs.gimp;
-        webcord = let
-          src = webcord;
-          # Because fetchzip is dumb and doesn't recognize the crx as zip.
-          vencordExtension =
-            prev.runCommand "vencord-extension" {
-              # I don't know how reproducible the url is...
-              VENCORD_CRX_URL = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=109.0&x=id%3Dcbghhgpcnddeihccjmnadmkaejncjndb%26installsource%3Dondemand%26uc";
-              sha256 = prev.lib.fakeHash;
-              outputHash = "sha256-lovKwzBa4IMKDZ4TR6lv9pzWV7Vjm1FXG8ZRv4dZ9IM=";
-              outputHashAlgo = "sha256";
-              outputHashMode = "recursive";
-              nativeBuildInputs = with prev; [curl p7zip];
-              downloadToTemp = false;
-            } ''
-              export TEMP="$(mktemp -d)"
-              curl -k "$VENCORD_CRX_URL" -L -o vencord.crx
-              mkdir -p "$out"
-              7z x vencord.crx -o"$out" -y
-              rm -rf "$out/_metadata"
-            '';
-        in
-          prev.webcord.overrideAttrs (old: {
-            inherit src;
-            version = "git";
-            patchPhase = ''
-              runHook prePatch
-              sed -i "361i session.defaultSession.loadExtension(\"${vencordExtension}\").then(() => console.log(\"Vencord loaded.\"));" "sources/code/common/main.ts"
-              sed -i "4i \ \ \ \ \ \ \ \ \"ignoreDeprecations\": \"5.0\"," "tsconfig.json"
-              runHook postPatch
-            '';
-            npmDeps = old.npmDeps.overrideAttrs (_old: {
-              inherit src;
-              outputHash = "sha256-Svkv6RiJrteCsZOn4prUMYguc4cuCktlCxb5yq63Fzw=";
-            });
-          });
+        # TODO: remove after updating nixpkgs
+        syncplay = prev.syncplay.overrideAttrs (old: {
+          buildInputs = (old.buildInputs or []) ++ [prev.qt5.qtwayland];
+        });
       })
     ];
   };
@@ -83,8 +49,8 @@
       pavucontrol
       obs-studio
       lxde.lxrandr
-      gimp-dev
-      webcord
+      # gimp-dev
+      # webcord-vencord
       element-desktop
       lutris
       android-studio
